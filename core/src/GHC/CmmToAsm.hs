d275e10960e1c0c2a448e21654d62843decc4ca8
Greg Pfeil <greg@technomadic.org>
2026-02-15 16:44:45 -0700

    Did `AsmCodeGen` not actually disappear?

--- core/src/GHC/CmmToAsm.hs
+++ core/src/GHC/CmmToAsm.hs
@@ -6,10 +6,6 @@ where
 
 #if MIN_VERSION_ghc(9, 0, 0)
 import "ghc" GHC.CmmToAsm as Upstream
-#elif MIN_VERSION_ghc(8, 8, 0)
-import "ghc" AsmCodeGen as Upstream
-#elif MIN_VERSION_ghc(8, 4, 0)
-import "this" Unresolved as Upstream
 #else
 import "ghc" AsmCodeGen as Upstream
 #endif
